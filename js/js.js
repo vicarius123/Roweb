637598efc65fdbb94be7793e98bc549e90985776
vicariusxz <cris_chong2@hotmail.com>
2018-08-21 17:27:48 +0300

    Notifications done

--- js/js.js
+++ js/js.js
@@ -9,18 +9,5 @@ $(document).ready(function(){
 
   });
 
-  $('.notif-icon').click(function(e){
-    console.log()
-    e.preventDefault();
-    if(e.target.localName == 'img')
-    $('.notifications-ctn').fadeToggle('100')
-  });
 
-  $(document).mouseup(function(e) {
-    var container = $(".notifications-ctn");
-    if (!container.is(e.target) && container.has(e.target).length === 0)
-    {
-      container.fadeOut('100');
-    }
-  });
 })
