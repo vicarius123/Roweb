140af06ce34afd9c7621036171d4172d9e974b77
vicariusxz <cris_chong2@hotmail.com>
2018-10-26 15:53:47 +0300

    Linegraph Done

--- js/js.js
+++ js/js.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
+  if(jQuery('#calendar').length > 0){
+    $( "#calendar" ).datepicker({
+      monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+      'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+      dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+      firstDay: 1,
+    });
+  }
 
 
-  $( "#calendar" ).datepicker({
-    monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
-    'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-    dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-    firstDay: 1,
-
-  });
-
-
+  $('i[data-toggle="tooltip"]') .tooltip({
+    offset: '-40px 6px'
+  })
 })
