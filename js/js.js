88b3d6f09dfddad317de0adc51ee4988c65015f8
vicariusxz <cris_chong2@hotmail.com>
2018-11-19 14:34:09 +0300

    update

--- js/js.js
+++ js/js.js
@@ -7,9 +7,27 @@ $(document).ready(function(){
       firstDay: 1,
     });
   }
-  $('i[data-toggle="tooltip"]') .tooltip({
-    offset: '-40px, 6px'
+  _html_pop = '<table class="linegraph-table table"> <thead> <th colspan="3"> История (курс 2) ЗЕТ №3 </th> </thead> <tbody> <tr> <td><span class="l__counter">1</span></td> <td>Штудирование.<br />Самостоятельная работа</td> <td><a href="#" class="btn-planning btn-yellow">Начать</a></td> </tr> <tr> <td><span class="l__counter">2</span></td> <td>Модульная лекция ср</td> <td><a href="#" class="btn-planning btn-yellow">Начать</a></td> </tr> <tr> <td><span class="l__counter">3</span></td> <td>Модульная лекция ср</td> <td><a href="#" class="btn-planning btn-yellow">Начать</a></td> </tr> <tr> <td><img src="/images/line--check.svg" /></td> <td>Модульная лекция ср</td> <td><a href="#" class="btn-green">Пройти снова</a></td> </tr> <tr> <td><span class="l__counter">5</span></td> <td>Модульная лекция ср</td> <td><a href="#" class="btn-yellow--empty">Продолжить</a></td> </tr> </tbody> </table>';
+  $('i[data-toggle="tooltip"]').tooltip({
+    offset: '-40px, 6px',
   })
+
+  _w = $(document).width();
+
+  if(_w >= 768){
+    $('.flex-linegraph .flex-fill').tooltip({
+      offset: '-40px, 6px',
+      trigger: 'click focus',
+      placement: 'auto',
+      html: true,
+      title: _html_pop,
+    })
+
+    $('.flex-linegraph .flex-fill').on('show.bs.tooltip	', function () {
+      $('.flex-linegraph .flex-fill').tooltip('hide')
+    });
+  }
+
   $('.mob-burger').click(function(e){
     e.preventDefault();
     $(this).toggleClass('active')
@@ -67,24 +85,31 @@ $(document).ready(function(){
   }
   getRandomInt(0, 3);
   //Linegraph popup
-  $('.flex-linegraph .flex-fill').click(function(e){
+  /* $('.flex-linegraph .flex-fill').click(function(e){
     e.preventDefault();
-
     _this = $(this);
-    _top = _this.offset().top+75;
-    _left = _this.offset().left;
+    _top = _this.offset().top-150;
+    _left = _this.offset().left-350;
     _w = $(document).width();
     _ww = $('.linegraph-pop').width();
     _www = (_ww+_left);
     _target = $(e.target);
-    $('.linegraph-pop').show();
-
+    if(_this.hasClass('active')){
+      _this.removeClass('active');
+      $('.linegraph-pop').hide();
+      $('.bg-linegraph--popup').removeClass('active');
+    }else{
+      $('.flex-linegraph .flex-fill.active').removeClass('active');
+      _this.addClass('active')
+      $('.linegraph-pop').show();
+      $('.bg-linegraph--popup').addClass('active');
+    }
     if(_www >= _w-100){
       $('.linegraph-pop').css({top:_top, left: (_left-_ww/1.5)})
+      $('.linegraph-pop').addClass('right')
     }else{
       $('.linegraph-pop').css({top:_top, left: _left})
+      $('.linegraph-pop').removeClass('right')
     }
-
-
-  })
+  })*/
 })
