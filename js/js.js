7aab8bfeaa08bfbc5a33c354ba0a5baddf0def1e
vicariusxz <cris_chong2@hotmail.com>
2018-11-19 16:33:57 +0300

    linegraph mob

--- js/js.js
+++ js/js.js
@@ -1,19 +1,5 @@
-$(document).ready(function(){
-  if($('#calendar').length > 0){
-    $( "#calendar" ).datepicker({
-      monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
-      'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-      dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-      firstDay: 1,
-    });
-  }
-  _html_pop = '<table class="linegraph-table table"> <thead> <th colspan="3"> История (курс 2) ЗЕТ №3 </th> </thead> <tbody> <tr> <td><span class="l__counter">1</span></td> <td>Штудирование.<br />Самостоятельная работа</td> <td><a href="#" class="btn-planning btn-yellow">Начать</a></td> </tr> <tr> <td><span class="l__counter">2</span></td> <td>Модульная лекция ср</td> <td><a href="#" class="btn-planning btn-yellow">Начать</a></td> </tr> <tr> <td><span class="l__counter">3</span></td> <td>Модульная лекция ср</td> <td><a href="#" class="btn-planning btn-yellow">Начать</a></td> </tr> <tr> <td><img src="/images/line--check.svg" /></td> <td>Модульная лекция ср</td> <td><a href="#" class="btn-green">Пройти снова</a></td> </tr> <tr> <td><span class="l__counter">5</span></td> <td>Модульная лекция ср</td> <td><a href="#" class="btn-yellow--empty">Продолжить</a></td> </tr> </tbody> </table>';
-  $('i[data-toggle="tooltip"]').tooltip({
-    offset: '-40px, 6px',
-  })
-
-  _w = $(document).width();
-
+function linetool(_w){
+  _html_pop = '<table class="linegraph-table table"> <thead> <th colspan="2"> История (курс 2) ЗЕТ №3 </th> <th class="text-right"> <a href="#" class="close-linegraph"><img src="/images/pop-close.svg"></a> </th> </thead> <tbody> <tr> <td><span class="l__counter">1</span></td> <td>Штудирование.<br />Самостоятельная работа</td> <td><a href="#" class="btn-planning btn-yellow">Начать</a></td> </tr> <tr> <td><span class="l__counter">2</span></td> <td>Модульная лекция ср</td> <td><a href="#" class="btn-planning btn-yellow">Начать</a></td> </tr> <tr> <td><span class="l__counter">3</span></td> <td>Модульная лекция ср</td> <td><a href="#" class="btn-planning btn-yellow">Начать</a></td> </tr> <tr> <td><img src="/images/line--check.svg" /></td> <td>Модульная лекция ср</td> <td><a href="#" class="btn-green">Пройти снова</a></td> </tr> <tr> <td><span class="l__counter">5</span></td> <td>Модульная лекция ср</td> <td><a href="#" class="btn-yellow--empty">Продолжить</a></td> </tr> </tbody> </table>';
   if(_w >= 768){
     $('.flex-linegraph .flex-fill').tooltip({
       offset: '-40px, 6px',
@@ -22,12 +8,35 @@ $(document).ready(function(){
       html: true,
       title: _html_pop,
     })
-
     $('.flex-linegraph .flex-fill').on('show.bs.tooltip	', function () {
       $('.flex-linegraph .flex-fill').tooltip('hide')
     });
+  }else{
+    $('.flex-linegraph .flex-fill').tooltip('dispose')
+    $('.flex-linegraph .flex-fill').click(function(e){
+      e.preventDefault;
+      window.location.href = "linegraph-pop.html";
+    })
   }
-
+}
+$(document).ready(function(){
+  _w = $(document).width();
+  linetool(_w);
+  if($('#calendar').length > 0){
+    $( "#calendar" ).datepicker({
+      monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+      'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+      dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+      firstDay: 1,
+    });
+  }
+  $('i[data-toggle="tooltip"]').tooltip({
+    offset: '-40px, 6px',
+  })
+  $(document).on('click', '.close-linegraph', function(e) {
+    e.preventDefault();
+    $('.flex-linegraph .flex-fill').tooltip('hide')
+  });
   $('.mob-burger').click(function(e){
     e.preventDefault();
     $(this).toggleClass('active')
@@ -84,32 +93,4 @@ $(document).ready(function(){
     $('.main-reg').css('background-image', 'url('+_imgs[_result]+')')
   }
   getRandomInt(0, 3);
-  //Linegraph popup
-  /* $('.flex-linegraph .flex-fill').click(function(e){
-    e.preventDefault();
-    _this = $(this);
-    _top = _this.offset().top-150;
-    _left = _this.offset().left-350;
-    _w = $(document).width();
-    _ww = $('.linegraph-pop').width();
-    _www = (_ww+_left);
-    _target = $(e.target);
-    if(_this.hasClass('active')){
-      _this.removeClass('active');
-      $('.linegraph-pop').hide();
-      $('.bg-linegraph--popup').removeClass('active');
-    }else{
-      $('.flex-linegraph .flex-fill.active').removeClass('active');
-      _this.addClass('active')
-      $('.linegraph-pop').show();
-      $('.bg-linegraph--popup').addClass('active');
-    }
-    if(_www >= _w-100){
-      $('.linegraph-pop').css({top:_top, left: (_left-_ww/1.5)})
-      $('.linegraph-pop').addClass('right')
-    }else{
-      $('.linegraph-pop').css({top:_top, left: _left})
-      $('.linegraph-pop').removeClass('right')
-    }
-  })*/
 })
